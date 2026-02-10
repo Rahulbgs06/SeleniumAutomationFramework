f2abab0a3855a7ed11f891b0fe30e9f4d97647cd
Rahul Kumar <rahulbgs06@gmail.com>
2026-02-10 15:36:49 +0530

    added taking data from excel

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -2,6 +2,9 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import Utils.Log;
 
@@ -9,32 +12,61 @@ public class LoginPage {
 	
 	private WebDriver driver;
 	
-	By usernameBox = By.id("Email");
-	By passwordBox = By .id("Password");
-	By loginbutton = By.xpath("//*[@id=\"main\"]/div/section/div/div[2]/div[1]/div/form/div[3]/button");
+	@FindBy(id="Email")
+	WebElement usernameBox;
+	
+	@FindBy(id="Password")
+	WebElement passwordBox;
+	
+	@FindBy(xpath="//*[@id=\"main\"]/div/section/div/div[2]/div[1]/div/form/div[3]/button")
+	WebElement loginbutton;
+	
+//	@FindBy(linkText = "Logout")
+//	WebElement linkLogout;
+
+	
+	/* Without Page Factory*/
+//	By usernameBox = By.id("Email");
+//	By passwordBox = By .id("Password");
+//	By loginbutton = By.xpath("//*[@id=\"main\"]/div/section/div/div[2]/div[1]/div/form/div[3]/button");
 	
 	
 	 public LoginPage(WebDriver driver)
 	 {
 		 this.driver = driver;
+		 PageFactory.initElements(driver, this); 
 	 }
+	 
 	 
 	
     public void enterUsername(String user) {
     	
-    	driver.findElement(usernameBox).clear();
-        driver.findElement(usernameBox).sendKeys(user);
+    	usernameBox.clear();
+    	usernameBox.sendKeys(user);
+//    	driver.findElement(usernameBox).clear);
+//        driver.findElement(usernameBox).sendKeys(user);
     }
 
     public void enterPassword(String pass) {
-    	driver.findElement(passwordBox).clear();
-        driver.findElement(passwordBox).sendKeys(pass);
+    	
+    	passwordBox.clear();
+    	passwordBox.sendKeys(pass);
+//    	driver.findElement(passwordBox).clear();
+//        driver.findElement(passwordBox).sendKeys(pass);
     }
 
     public void clickLogin() {
     	
-        driver.findElement(loginbutton).click();
+    	loginbutton.click();
+//        driver.findElement(loginbutton).click();
     }
+    
+
+
+//    public boolean isLogoutVisible() {
+//        return linkLogout.isDisplayed();
+//    }
+
 	
 	
 }
--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -1,50 +1,87 @@
 package Tests;
 
+import java.io.IOException;
+
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import Base.BaseTest;
 import Pages.LoginPage;
+import Utils.ExcelUtils;
 import Utils.ExtentReportManager;
 import Utils.Log;
 
 public class LoginTest extends BaseTest {
 
-	@Test
-	public void testlogin() {
+	@DataProvider(name = "LoginData")
+	public Object[][] getLoginData() throws IOException {
+
+		String filePath = System.getProperty("user.dir") + "/Testdata/TestData.xlsx";
+		ExcelUtils.loadExcel(filePath, "Sheet1");
+		int rowCount = ExcelUtils.getRowCount();
+		Object[][] data = new Object[rowCount - 1][2];
+
+		for (int i = 1; i < rowCount; i++) {
+
+			data[i - 1][0] = ExcelUtils.getCellData(i, 0); // Username
+			data[i - 1][1] = ExcelUtils.getCellData(i, 1); // Password
+		}
+		ExcelUtils.closeExcel();
+		return data;
+	}
+
+	@Test(dataProvider = "LoginData")
+	public void testlogin(String username, String password) {
 
 		Log.info("Starting Loging Test .....");
 
-		test = test = extent.createTest("Login test");
+		test = extent.createTest("Login test" + username);
 		test.info("start Login test");
 		test.info("navigating to url");
+		/*
+		 * Intializing driver and all page factory elements by creating object of that
+		 * class
+		 */
 		LoginPage loginpage = new LoginPage(driver);
 
 		Log.info("credentials .....");
 		test.info("adding credentials");
 
-		loginpage.enterUsername("admin@yourstore.com");
+//		loginpage.enterUsername("admin@yourstore.com");
+//
+//		loginpage.enterPassword("admin");
 
-		loginpage.enterPassword("admin");
+		loginpage.enterUsername(username);
+
+		loginpage.enterPassword(password);
 
 		Log.info("Logging In .....");
 		test.info("click on login button");
 		loginpage.clickLogin();
 
-		System.out.println("Title :" + driver.getTitle());
-
+		System.out.println("Title :" + driver.getCurrentUrl());
+		
+		
 		test.info("verfying Page title");
-		Assert.assertEquals(driver.getTitle(), "nopCommerce demo store. Login");
+		
+		Assert.assertTrue(driver.getCurrentUrl().contains("https://admin-demo.nopcommerce.com/admin/"));
+//		Assert.assertEquals(driver.getTitle(), "Dashboard / nopCommerce administration");
 		test.pass("Login Successfull");
 
+		
+//		test.info("verifying login");
+//		Assert.assertTrue(loginpage.isLogoutVisible());
+//		test.pass("Login Successful");
+
 	}
-	
-	@Test
+
+/*	@Test
 	public void testloginInvalid() {
 
 		Log.info("Starting Loging Test .....");
 
-		test = test = extent.createTest("Login test Invalid");
+		test = extent.createTest("Login test Invalid");
 		test.info("start Login test");
 		test.info("navigating to url");
 		LoginPage loginpage = new LoginPage(driver);
@@ -61,10 +98,12 @@ public class LoginTest extends BaseTest {
 		loginpage.clickLogin();
 
 		System.out.println("Title :" + driver.getTitle());
-
+		
 		test.info("verfying Page title");
-		Assert.assertEquals(driver.getTitle(), "Just a moment...");
+		Assert.assertEquals(driver.getTitle(), "nopCommerce demo store. Login");
 		test.pass("Login Successfull");
 
-	}
+
+	}   
+*/
 }
